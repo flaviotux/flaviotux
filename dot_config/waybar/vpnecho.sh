4b60646428b8686bab6732ace6c7e39f815471df
Flávio Ribeiro <flavio.ribeiro@luizalabs.com>
2022-03-09 19:40:16 -0300

    chore(style): chromium waybar

--- dot_config/waybar/vpnecho.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ping -q -w 3 -c 1 10.0.2.1>/dev/null \
-&& echo '{"text":"Connected","class":"connected","percentage":100}' \
-|| echo '{"text":"Disconnected","class":"disconnected","percentage":0}'
